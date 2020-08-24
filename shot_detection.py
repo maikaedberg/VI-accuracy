32838b84161a6600b80644017b7622059e7974fd
maikaedberg <69947279+maikaedberg@users.noreply.github.com>
2020-08-25 02:17:01 +0900

    Update shot_detection.py

--- shot_detection.py
+++ shot_detection.py
@@ -1,11 +1,7 @@
 import json
 import videoindexer
 import moviegraphs
-import numpy as np
-
-vid_dir = '/porject/mg_videoinfo/video_boundaries/'
-json_dir = '/project/vi_json/'
-
+import xlsxwriter
 
 def recall_precision_shots(movie_id):
     """returns the recall and precision of the shot detection software,
@@ -19,9 +15,9 @@ def recall_precision_shots(movie_id):
         data = json.load(f)
         
     MG = [i for i in moviegraphs.load_cuts(vid_dir, movie_id) if s < i < e]
-    VI = [i for i in videoindexer.load_cuts(data) if s  < i < e]
+    VI = [i for i in videoindexer.load_cuts(data) if s < i < e]
     
-    count= 0
+    count = 0
     for i in range(len(MG)):
         if i == 0: prev = 0
         for j in range(prev, len(VI)):
@@ -30,5 +26,21 @@ def recall_precision_shots(movie_id):
                 count += 1
     return (round(count / len(VI), 3), round(count / len(MG), 3))
 
+workbook = xlsxwriter.Workbook('shot_boundaries.xlsx')
+worksheet = workbook.add_worksheet()
 
-            
+worksheet.write(0, 0, 'IMDb id')
+worksheet.write(0, 1, 'precision')
+worksheet.write(0, 2, 'recall')
+
+row = 1
+with open('movie_list.txt', 'r') as f:
+    for l in f:
+        movie_id = l.split('\t')[0]
+        p, r = recall_precision_shots(movie_id)
+        worksheet.write(row, 0, movie_id)
+        worksheet.write(row, 1, '{}%'.format(round(p*100, 1))
+        worksheet.write(row, 2, '{}%'.format(round(r*100, 1))
+        row += 1
+        
+workbook.close()
