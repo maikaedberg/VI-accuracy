77b24957b958cefe9f0cc10010f808f3ca222f6e
maikaedberg <69947279+maikaedberg@users.noreply.github.com>
2020-08-25 02:08:43 +0900

    Update characters.py

--- characters.py
+++ characters.py
@@ -1,9 +1,7 @@
-import json
 import moviegraphs
+import videoindexer
 from imdb import IMDb
-
-json_dir = '/project/vi_json/'
-
+import xlsxwriter
 
 def cast_list(movie_id):
     """given the imdb id of a movie,
@@ -25,23 +23,10 @@ def cast_list(movie_id):
             
     return roles
 
-def get_character_app_dict(name):
-    """returns a dictionary where the key is the actor 
-    name in the VideoIndexer dataset, and the value is the 
-   intervals (in seconds) at which they appear"""
-    
-    with open(json_dir + name + '.json') as infile:
-        data = json.load(infile)
-    char_app = dict()
-    for i in data["summarizedInsights"]["faces"]:
-        char_app[i["name"]] = [(j['startSeconds'],j['endSeconds']) for j in i["appearances"]]
-    return char_app
-
-
 def chars_in_scenes(scenes, chars):
     """return a list of actors given a list of tuples of start and end 
     seconds of each scene, so characters[i] gives all the actors 
-    that appear between s_e[i][0] and s_e[i][1], according to VI"""
+    that appear between scenes[i][0] and scenes[i][1], according to VI"""
     res  = []
     for (s, e) in scenes:
         characters = []
@@ -50,35 +35,15 @@ def chars_in_scenes(scenes, chars):
                 # if the two tuples (s,e) and (a[0], a[1]) overlap
                 if(a[1] >= s and a[0] <= e): 
                     characters.append(c) 
-                    break
         res.append(characters)
     return res
-
-def get_chars_vi(test_id):
-    """returns a list such that list[i] gives
-    the set of actors found in the i-th Mg scene"""
-    scenes = moviegraphs.load_scenes(test_id, all_mg)
-    chars = get_character_app_dict(test_id)
-    c = chars_in_scenes(scenes, chars)
-    actors = cast_list(test_id)
-    
-    for i in range(len(c)):
-        unknown = []
-        for j in range(len(c[i])):
-            if c[i][j] in actors:
-                c[i][j] = actors[c[i][j]]
-            else:
-                unknown.append(c[i][j])
-                c[i][j] = 'Unknown'
-            
-    return [set(i) for i in c]
 
 def char_precision(test_id, all_mg):
     """returns the precision of the face identification
     software comparing the VI results to the information 
     on the MG dataset"""
     scenes = moviegraphs.load_scenes(test_id, all_mg)
-    chars = get_character_app_dict(test_id)
+    chars = videoindexer.get_character_app_dict(test_id)
     chars_vi = chars_in_scenes(scenes, chars)
     
     actors = cast_list(test_id)
@@ -95,5 +60,20 @@ def char_precision(test_id, all_mg):
                 if chars_vi[i][j] in res[i]:
                     correct += 1
                 total += 1
-    return (correct / total) if total != 0 else 0
+    return round((correct / total * 100), 1) if total != 0 else 0
 
+workbook = xlsxwriter.Workbook('facial_identification_precision.xlsx')
+worksheet = workbook.add_worksheet()
+worksheet.write(0, 0, 'IMDb id')
+worksheet.write(0, 1, 'precision')
+
+row = 1
+with open('movie_list.txt', 'r') as f:
+    for l in f:
+        movie_id = l.split('\t')[0]
+        precision = char_precision(movie_id, all_mg)
+        worksheet.write(row, 0, movie_id)
+        worksheet.write(row, 1, '{}%'.format(precision))
+        row += 1
+        
+workbook.close()
